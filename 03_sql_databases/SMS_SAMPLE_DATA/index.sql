7cb3d6214e20327a3fff912948d92ce04486a5d5
unknown <manojrawat87634@gmail.com>
2026-04-12 16:03:13 +0530

    inital commit

--- 03_sql_databases/SMS_SAMPLE_DATA/index.sql
+++ 03_sql_databases/SMS_SAMPLE_DATA/index.sql
@@ -1,12 +1,11 @@
-create table student (
-     id bigint primary key auto_increment,
-     name varchar(100) not null,
-     email varchar(200) not null unique,
-     phone varchar(20) not null unique,
-     created_at datetime default now(),
-      index email_idx(email),
-      index phone_idx(phone));
-
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(200) NOT NULL UNIQUE,
+    phone VARCHAR(20) NOT NULL UNIQUE,
+    role ENUM('student','faculty','admin'),
+    created_at DATETIME DEFAULT NOW()
+);
 
 create table courses
  ( id bigint primary key auto_increment, name varchar(200) not null unique);
@@ -41,10 +40,56 @@ CREATE TABLE course_modules (
 
 CREATE TABLE enrollments (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    student_id BIGINT,
+    user_id BIGINT,
     course_id BIGINT,
     enrolled_at DATETIME DEFAULT NOW(),
 
-    FOREIGN KEY (student_id) REFERENCES student(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (course_id) REFERENCES courses(id)
+);
+
+
+CREATE TABLE batches (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    course_id BIGINT,
+    name VARCHAR(100),
+
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+);
+
+
+CREATE TABLE batch_students (
+    batch_id BIGINT,
+    user_id BIGINT,
+    PRIMARY KEY (batch_id, user_id),
+    FOREIGN KEY (batch_id) REFERENCES batches(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE batch_schedule (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    batch_id BIGINT,
+    module_id BIGINT,
+    user_id bigint,
+    day_of_week ENUM('Mon','Tue','Wed','Thu','Fri','Sat','Sun'),
+    start_time TIME,
+    end_time TIME,
+    FOREIGN KEY (batch_id) REFERENCES batches(id),
+    FOREIGN KEY (module_id) REFERENCES modules(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE classes (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    batch_id BIGINT,
+    module_id BIGINT,
+    faculty_id BIGINT,
+    class_date DATE,
+    start_time TIME,
+    end_time TIME,
+    status ENUM('scheduled','completed','cancelled') DEFAULT 'scheduled',
+
+    FOREIGN KEY (batch_id) REFERENCES batches(id),
+    FOREIGN KEY (module_id) REFERENCES modules(id),
+    FOREIGN KEY (faculty_id) REFERENCES users(id)
 );
