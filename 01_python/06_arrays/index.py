d35ba24def41aa7f8b92672543314f54bf9b74e9
manojrawat87634 <manojrawat87634@gmail.com>
2026-06-09 12:58:38 +0530

    revision of arrays

---
+++ 01_python/06_arrays/index.py
@@ -0,0 +1,36 @@
+# arr = [1, 12, 3, 4]
+
+# arr.append(21)
+# print(arr)
+# arr.reverse()
+# # arr.clear()
+# # print(arr)
+# # arr.pop()
+# # print(arr)
+# # arr.insert(1, 2)
+# # arr.remove(2)
+# # arr.sort()
+# print(arr)
+# # arr.pop()
+# # print(arr)
+# # arr.pop()
+# # print(arr)
+# # arr.pop()
+# # print(arr)
+
+data = {
+    "name" : "manoj",
+    "email" : "manoj@gmail.com"
+}
+
+
+print(data.values())
+print(data.keys())
+data.clear()
+data.update()
+print(data)
+# print(data['name'])
+# 
+print(data.get("name"))
+print(data.values())
+print(data.values())
---
+++ 01_python/06_arrays/topics/index.py
@@ -0,0 +1,26 @@
+arr = [
+    {
+        "que0" : "Who is the capital of india?",
+        "options" : ["Delhi", "mumbai", "patna"],
+        "ans" : 0
+    },
+    {
+        "que0" : "Who is the capital of india?",
+        "options" : ["Delhi", "mumbai", "patna"],
+        "ans" : 0
+    },
+    {
+        "que0" : "Who is the capital of india?",
+        "options" : ["Delhi", "mumbai", "patna"],
+        "ans" : 0
+    },
+]
+
+for i in arr:
+    print(i.get("que0"))
+    print(i.get('options'))
+    ch = input("Enter ch")
+    if (ch == i.get("options")[i.get("ans")]):
+        print("Correct Ans")
+    else:
+        print("Incorrect")
