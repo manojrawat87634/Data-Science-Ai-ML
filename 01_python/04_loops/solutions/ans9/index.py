6c12ae6a27bffb6258df9ec2ba28a146a3d67f2e
manojrawat87634 <manojrawat87634@gmail.com>
2026-06-20 10:50:45 +0530

    while loops

--- 01_python/04_loops/solutions/ans9/index.py
+++ 01_python/04_loops/solutions/ans9/index.py
@@ -1,17 +1,7 @@
 n = int(input("Enter a number: "))
-i = 2
-is_prime = True
+i = 1
 
-if n <= 1:
-    is_prime = False
-else:
-    while i < n:
-        if n % i == 0:
-            is_prime = False
-            break
-        i += 1
-
-if is_prime:
-    print("Prime number")
-else:
-    print("Not a prime number")
+while i <= n:
+    if n % i == 0:
+        print(i)
+    i += 1
